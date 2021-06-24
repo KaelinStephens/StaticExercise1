8ea67e6587b7edcc0cd5e4f1974666b596c9547a
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-24 07:45:35 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int tempscale = TempScale("Is your temperature in Celcius or Fahreneit?");
+            TempNumber(tempscale);
+        }
+        public static int TempScale(string prompt)
+        {
+            Console.WriteLine(prompt);
+            while (true)
+            {
+                int choice;
+                Console.WriteLine("1: Celcius");
+                Console.WriteLine("2: Fahrenheit");
+                string input = Console.ReadKey().KeyChar.ToString();
+
+                if (Int32.TryParse(input, out choice))
+                {
+                    // check if number is between 0 and choices.Length
+                    if (choice == 1)
+                    {
+                        Console.WriteLine($"Celcius");
+                        return choice;
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine($"Fahrenheit");
+                        return choice;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You must choose between 1 and 2");
+                        continue;
+                    }
+                    //   if yes return number
+                    //   if no output error and continue
+
+                }
+               
+            }
+
+        }
+
+        public static void TempNumber(int tempscale)
+        {
+            Console.WriteLine("What is the temperature to be converted?");
+            string input = Console.ReadLine();
+
+            double convTemp;
+            if (Double.TryParse(input, out convTemp))
+            {
+                // check if number is between 0 and choices.Length
+                if (tempscale == 1)
+                {
+                    double newTemp = TempConverter.CelsiusToFahrenheit(convTemp);
+                    Console.WriteLine($"{input} degrees C is {newTemp} degrees F.");
+
+                }
+                else
+                {
+                    double newTemp = TempConverter.FahrenheitToCelsius(convTemp);
+                    Console.WriteLine($"{input} degrees F is {newTemp} degrees C.");
+                }
+
+            }
+         
+            
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double tempF)
+        {
+            double fToC = (tempF - 32) * 5 / 9;
+            return fToC;
+        }
+        public static double CelsiusToFahrenheit(double tempC)
+        {
+            double cToF = (tempC * 9 / 5) + 32;
+            return cToF;
+        }
+    }
+}
